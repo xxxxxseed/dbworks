4a00bd4ca621ce346b25c42ac8c35a185b611f3c
xxxxxseed <xxxxxseed@gmail.com>
2022-08-05 18:18:32 +0900

    member_board update

--- jweb/member_board.sql
+++ jweb/member_board.sql
@@ -8,8 +8,24 @@ CREATE TABLE t_member(
     joindate    DATE DEFAULT SYSDATE
 );
 
+CREATE TABLE t_board(
+    bnum    NUMBER(4) PRIMARY KEY,
+    title   VARCHAR2(100) NOT NULL,
+    content VARCHAR2(2000) NOT NULL,
+    regdate DATE DEFAULT SYSDATE,
+    memberId    VARCHAR2(20),
+    CONSTRAINT FK_MemberBoard FOREIGN KEY (memberId)
+    REFERENCES t_member(memberId)
+);
+
+-- 시퀀스 생성
+CREATE SEQUENCE b_seq;
+
 INSERT INTO t_member VALUES ('sky', 'm12345', '강하늘', '남', SYSDATE);
+INSERT INTO t_board 
+VALUES (b_seq.nextval, '제목입니다', '글 내용입니다', SYSDATE, 'cloudy');
 
 SELECT * FROM t_member;
+SELECT * FROM t_board;
 
 DESC t_member;
